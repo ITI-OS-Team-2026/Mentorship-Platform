ce37aa4b4e7ac5ce326bb0d5c4167d611c6daf47
Ramadan-Elgamal <ramadan.reda12345@gmail.com>
2026-06-30 11:18:30 +0300

    feat: added the postman schema and the postman collection

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -8,16 +8,4 @@ router.post('/register', registerUser);
 router.post('/login', loginUser);
 
 
-// Test routes to verify our auth middleware is working correctly
-
-// // NEW: A protected test route that ONLY logged-in users can hit
-// router.get('/me', protect, (req, res) => {
-//   res.status(200).json(req.user);
-// });
-
-// // NEW: A highly restricted route that ONLY Admins can hit
-// router.get('/admin-only', protect, authorizeRoles('Admin'), (req, res) => {
-//   res.status(200).json({ message: "Welcome to the secret admin club." });
-// });
-
 export default router;
